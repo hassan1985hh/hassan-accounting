5423b565f4b4eaf464d44267425d439c56ceca2f
hassan1985hh <hassanhosen85@gmail.com>
2026-09-19 04:41:05 +0300

    Add files via upload

---
+++ Hassan-iPhone-Capacitor/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.hassan.accounting',
+  appName: 'برنامج حسان المحاسبي',
+  webDir: 'www'
+};
+
+export default config;
